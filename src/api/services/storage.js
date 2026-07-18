1552817269af111c9da6cc03740bc6f6dbdf8738
valentinpetrov420 <valentin.petrov420@abv.bg>
2026-07-18 10:20:57 +0300

    changed node nesting utility function to sort by order property, default order is 0 if order is undefined

--- src/api/services/storage.js
+++ src/api/services/storage.js
@@ -1,13 +1,17 @@
 export default function nestNodes(flatNodes) {
-    const roots = flatNodes.filter(node => node.parentId === null);
+    const roots = flatNodes
+        .filter(node => node.parentId === null)
+        .sort((a, b) => (a.order ?? 0) - (b.order ?? 0));
+
     const children = flatNodes.filter(node => node.parentId !== null);
 
     const nested = roots.map(root => {
         return {
             ...root,
-            items: children.filter(child => child.parentId === root.id)
+            items: children
+                .filter(child => child.parentId === root.id)
+                .sort((a, b) => (a.order ?? 0) - (b.order ?? 0))
         }
     });
-
     return nested;
 }
