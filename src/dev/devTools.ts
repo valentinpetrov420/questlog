97720be0e5dbe270f26cfa5fd1cb166a15e1501a
valentinpetrov420 <valentin.petrov420@abv.bg>
2026-07-23 14:33:48 +0300

    deleted deprecated DevTools file that contained dead code of old localstorage functions

--- src/dev/devTools.ts
+++
@@ -1,9 +0,0 @@
-import mockData from "../mockdata";
-
-export function __clearStorage(setLists) {
-    setLists([]);
-    localStorage.setItem("lists", JSON.stringify([]));
-}
-export function __loadMockStorage(setLists) {
-    setLists(mockData);
-}
