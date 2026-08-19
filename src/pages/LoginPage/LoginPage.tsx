efd475fb0ae8c34cdf92b2f4b764a7d6b3d77399
valentinpetrov420 <valentin.petrov420@abv.bg>
2026-08-19 13:57:02 +0300

    added dev environment only Continue as Guest option button in LoginPage, has an empty function

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -3,11 +3,20 @@ import GoogleSignInButton from "../../components/GoogleSignInButton/GoogleSignIn
 
 export default function LoginPage() {
 
+    function continueAsGuest() {
+        //interact with authcontext
+    }
+
     //todo: implement Continue as Guest app behavior, gating firestore services if theres no user (use localStorage instead)
     return <div className="login-card-container">
         <h1 className="login-branding">Questlog</h1>
         <p className="login-description">Organize anything as pages or tasks - nested, shareable, yours.</p>
-        <GoogleSignInButton/>
+        <GoogleSignInButton />
+        {import.meta.env.DEV && (
+            <div className='login-guest'>
+                <p>or</p>
+                <span className='continue-as-guest' onClick={continueAsGuest}>Continue as Guest</span>
+            </div>
+        )}
     </div>
-
 }
