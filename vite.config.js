2f3ea125fba2110a6c35ca4feee2cbfc70dc84eb
valentinpetrov420 <valentin.petrov420@abv.bg>
2026-09-01 16:49:41 +0300

    added jestdom, react, user-event testing libraries
    created test/setup.js
    
    fixed vite.config.js to be able to run jsdom for .tsx files and node .ts files
    
    wrote tests for ProgressBar.tsx (dumb presentational component that displays a given percentage and sets the fill's width to the same percentage)

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,29 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  test: {
+    globals: true,
+
+    projects: [
+      {
+        plugins: [react()],
+        test: {
+          name: 'node',
+          environment: 'node',
+          setupFiles: ['./src/test/setup.ts'],
+          include: ['**/*.test.ts'],
+        },
+      },
+      {
+        plugins: [react()],
+        test: {
+          name: 'jsdom',
+          environment: 'jsdom',
+          setupFiles: ['./src/test/setup.ts'],
+          include: ['**/*.test.tsx'],
+        },
+      },
+    ],
+  },
 })
